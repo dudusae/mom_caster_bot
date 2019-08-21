57e0859d5f881a59212c076f2b9bc1b3613efaec
dudusae <dudusae@gmail.com>
2019-08-21 20:51:01 +0900

    동정보 없을시 시군구 정보 조회

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
                           ConversationHandler)
-
 import logging
 import datetime
 import config
@@ -15,8 +14,6 @@ db = client.mommbot
 my_token = config.my_token
 my_key_weather = config.my_key_weather
 
-
-# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
@@ -24,30 +21,11 @@ logger = logging.getLogger(__name__)
 
 GENDER, LOCATION, LOCATION_SELECT, WEATHER = range(4)
 
-# 사용자 지역의 날씨 정보 호출하기
-#   : 사용자의 location_id와 일치하는 기상 위치 xy값을 db.location으로 부터 불러와서
-#     (신)동네예보정보조회서비스 API에 날씨 정보를 호출한다.(오늘 오전 6시 기준)
-def request_weather(chat_id):
-    location_id = db.user.find_one({'chat_id':chat_id})['location_id']
-    location_x = db.location.find_one({'_id':location_id})['격자 X']
-    location_y = db.location.find_one({'_id':location_id})['격자 Y']
-    print(location_x,location_y)
 
-    now = datetime.datetime.now()
-    today = now.strftime('%Y%m%d')
-    time = '0600'
-
-    r = requests.get(
-        'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib?ServiceKey='
-        +my_key_weather+'&base_date='+today+'&base_time='+time
-        +'&nx='+str(location_x)+'&ny='+str(location_y)+'&pageNo=1&numOfRows=1&_type=json')
-    rjson = r.json()
-    print(rjson['response']['body']['items']['item'])
+# 챗봇함수 시작 ---------------------------------------------------------------
 
 
-####### 챗봇 함수 ####
-
-# /start 명령을 입력하면 '누구니'라고 물어본다. '딸' '아들' 선택 버튼이 나타난다.
+# /start 명령 : '누구니'라고 물어본다. '딸' '아들' 선택 버튼이 나타난다.
 def start(update, context):
     reply_keyboard = [[emojize('딸이얌:heart:', use_aliases=True),
                        emojize('아들이요:alien:', use_aliases=True)]]
@@ -58,6 +36,7 @@ def start(update, context):
     return GENDER
 
 
+# 딸, 아들 정보 저장하고 지역 물어보기
 def gender(update, context):
     chat_id = update.message.chat_id
 
@@ -75,15 +54,17 @@ def gender(update, context):
 
     return LOCATION
 
+
 def gender_error(update, context):
     update.message.reply_text('누구냐? 딸이냐 아들이냐'+emojize(':shit:', use_aliases=True))
 
 
+# 동이름 검색하고 사용자 지역정보 저장하기
 def location(update, context):
     chat_id = update.message.chat_id
     chat_txt = update.message.text
 
-    search_result = db.location.find({'3단계': {'$regex': '.*' + chat_txt + '.*'}})
+    search_result = db.location.find({'3단계': {'$regex':chat_txt+'$'}})
     search_result_list = []
 
     for i in search_result:
@@ -112,21 +93,61 @@ def location(update, context):
 
     # 3 입력한 지역정보를 찾을 수 없을 때 다시 물어봄
     else:
-        update.message.reply_text('엄마는 잘 모르는 곳인 것 같구나'+emojize(':eyes:', use_aliases=True)
-                                  +'\n가까운 다른 동네 이름을 알려줘')
+        update.message.reply_html('엄마는 잘 모르는 곳인 것 같구나'+emojize(':eyes:', use_aliases=True)
+                                  +'\n<b>시나 군</b> 이름으로 알려줘.')
         return LOCATION
 
 
 def location_null(update, context):
-    update.message.reply_text('이중에 없다구?'+ emojize(':speak_no_evil:', use_aliases=True)
-                              + '\n엄마가 모르는 동네인가 보다.\n가까운 다른 동네 이름을 알려줘.')
+    update.message.reply_html('이중에 없다구?'+ emojize(':speak_no_evil:', use_aliases=True)
+                              + '\n엄마가 모르는 동네인가 보다.\n<b>시나 군</b> 이름으로 알려줘.')
     return LOCATION
+
+
+# 동정보가 없을 경우 시,군,구로 조회
+def location_city(update, context):
+    chat_id = update.message.chat_id
+    chat_txt = update.message.text
+
+    search_result = db.location.find({'$and': [{'2단계': {'$regex':chat_txt+'$'}},{'3단계':''}]})
+    search_result_list = []
+
+    for i in search_result:
+        search_result_list.append(i)
+
+    # 1 검색결과가 1개일 때 DB에 저장하고 저장했다고 알려줌
+    if len(search_result_list) == 1:
+        reply_keyboard = [[emojize('아니요 알려주세요:raised_hands:', use_aliases=True),
+                           emojize('네 이미 알고 있죠:v:', use_aliases=True)]]
+
+        db.user.find_one_and_update({'chat_id': chat_id},
+                                    {'$set': {'location_id': search_result_list[0]['_id']}})
+        update.message.reply_text(search_result_list[0]['2단계']+'이구나 기억했다.\n오늘 일기예보는 확인했니?',
+                                  reply_markup=ReplyKeyboardMarkup(reply_keyboard,one_time_keyboard=True))
+        return WEATHER
+
+    # 2 검색결과가 2개 이상일 때 다시 확인한다
+    elif len(search_result_list) >= 2:
+        reply_keyboard = []
+        for i in search_result_list:
+            reply_keyboard.append(i['1단계']+' '+i['2단계'])
+        reply_keyboard.append('이 중에는 없어요')
+        update.message.reply_text(chat_txt + ', 여러 곳이있네. \n이중에 어디를 말하는거니?',
+                                  reply_markup=ReplyKeyboardMarkup.from_column(reply_keyboard, one_time_keyboard=True))
+        return LOCATION_SELECT
+
+    # 3 입력한 지역정보를 찾을 수 없을 때 다시 물어봄
+    else:
+        update.message.reply_html('엄마는 잘 모르는 곳인 것 같구나'+emojize(':eyes:', use_aliases=True)
+                                  +'\n<b>다른 지명</b>으로 알려줘.')
+        return LOCATION
 
 
 def location_error(update, context):
     update.message.reply_html('엄마는 나이가 들어서 정확하게 알려줘야해.'+emojize(':see_no_evil:', use_aliases=True)
                               +'\n정확한 <b>동 이름</b>을 다시 알려주렴.')
     return LOCATION
+
 
 def location_save(update, context):
     chat_id = update.message.chat_id
@@ -135,17 +156,70 @@ def location_save(update, context):
     reply_keyboard = [[emojize('아니요 알려주세요:raised_hands:', use_aliases=True),
                        emojize('이미 알고 있죠:v:', use_aliases=True)]]
 
-    search_result = db.location.find_one({'$and': [{'1단계': location_value[0]},
-                                               {'2단계': location_value[1]},
-                                               {'3단계': location_value[2]}]})
-
+    if len(location_value) >= 3 :
+        search_result = db.location.find_one({'$and': [{'1단계': location_value[0]},
+                                                   {'2단계': location_value[1]},
+                                                   {'3단계': location_value[2]}]})
+        update.message.reply_text(search_result['1단계']+' '
+                                  + search_result['2단계']+' '
+                                  + search_result['3단계']
+                                  + '이구나 기억했다.')
+    else :
+        search_result = db.location.find_one({'$and': [{'1단계': location_value[0]},
+                                                   {'2단계': location_value[1]}]})
+        update.message.reply_text(search_result['1단계']+' '
+                                  + search_result['2단계']+' '
+                                  + '이구나 기억했다.')
     db.user.find_one_and_update({'chat_id': chat_id},
                                 {'$set': {'location_id': search_result['_id']}})
-    update.message.reply_text(search_result['3단계'] + '이구나 기억했다.\n오늘 일기예보는 확인했니?',
+    update.message.reply_text('오늘 일기예보는 확인했니?',
                               reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
     return WEATHER
 
+
+# 날씨를 알려준다
+def weather(update, context):
+    # user = update.message.from_user
+    # print(user['id'])
+    chat_id = update.message.chat_id
+    print(chat_id)
+    location_id = db.user.find_one({'chat_id': chat_id})['location_id']
+    location_x = db.location.find_one({'_id': location_id})['격자 X']
+    location_y = db.location.find_one({'_id': location_id})['격자 Y']
+    print(location_x, location_y)
+
+    now = datetime.datetime.now()
+    today = now.strftime('%Y%m%d')
+    time = '0600'
+
+    r = requests.get(
+        'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib?ServiceKey='
+        + my_key_weather + '&base_date=' + today + '&base_time=' + time
+        + '&nx=' + str(location_x) + '&ny=' + str(location_y) + '&pageNo=1&numOfRows=1&_type=json')
+    rjson = r.json()
+    weather = rjson['response']['body']['items']['item']
+    print(weather)
+    update.message.reply_text('그래, 날씨를 알려줄께.\n'+weather['category']+':'+str(weather['obsrValue']),
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+
+def weather_skip(update, context):
+    chat_id = update.message.chat_id
+    gender = db.user.find_one({'chat_id':chat_id})['gender']
+    update.message.reply_text('우리'+gender+'이 웬일이래.'+emojize(':innocent:', use_aliases=True)
+                              +'\n그래 내일 아침부터 문자 넣을게.\n'
+                              +'주소 바뀌면 /location\n'
+                              +'날씨 궁금하면 /weather 라고 엄마한테 말해줘\n'
+                              +'영어라고 어려워말고'+emojize(':kissing_heart:', use_aliases=True),
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+
+# /location 명령 : 지역정보 업데이트
 def location_update(update, context):
     chat_id = update.message.chat_id
 
@@ -155,23 +229,6 @@ def location_update(update, context):
     return LOCATION
 
 
-def weather(update, context):
-    update.message.reply_text('그래, 날씨를 알려주마. 개발이 다 되면...')
-
-    return ConversationHandler.END
-
-def weather_skip(update, context):
-    chat_id = update.message.chat_id
-    gender = db.user.find_one({'chat_id':chat_id})['gender']
-    update.message.reply_text('우리'+gender+'이 웬일이래.'+emojize(':innocent:', use_aliases=True)
-                              +'\n그래 내일 아침부터 문자 넣을게.\n'
-                              +'주소 바뀌면 /location\n'
-                              +'날씨 궁금하면 /weather 라고 엄마한테 말해줘\n'
-                              +'영어라고 어려워말고')
-
-    return ConversationHandler.END
-
-
 def cancel(update, context):
     update.message.reply_text('그래 나중에 연락해'+emojize(':ok_woman:', use_aliases=True),
                               reply_markup=ReplyKeyboardRemove())
@@ -179,13 +236,12 @@ def cancel(update, context):
     return ConversationHandler.END
 
 
-
+# 정의되지 않은 메시지에 대한 처리 : 받은 메시지를 돌려준다
 def echo(update, context):
-    """Echo the user message."""
     update.message.reply_text(update.message.text)
 
 
-
+# 챗봇 실행
 
 def main():
     my_token = config.my_token
@@ -201,9 +257,10 @@ def main():
                      MessageHandler(Filters.regex('[^(딸|아들)]'), gender_error)],
 
             LOCATION: [MessageHandler(Filters.regex('(동|읍|면)$'), location),
+                              MessageHandler(Filters.regex('(시|군|구)$'), location_city),
                        MessageHandler(Filters.regex('[^(동|읍|면)$]'), location_error)],
 
-            LOCATION_SELECT: [MessageHandler(Filters.regex('(동|읍|면)$'), location_save),
+            LOCATION_SELECT: [MessageHandler(Filters.regex('(동|읍|면|시|군|구)$'), location_save),
                               MessageHandler(Filters.regex('[^(동|읍|면)$]'), location_null)],
 
             WEATHER: [MessageHandler(Filters.regex('^(아니|아직)'), weather),
@@ -226,5 +283,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-# 답변을 받아서 DB에 저장하고, DB를 검색하는 변수로 활용해야 함.
